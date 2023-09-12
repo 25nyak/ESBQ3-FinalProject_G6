3559280de0513b12122ac5dc71697f2d8cdf68c3
jrocha-v <jrocha-v@student.42porto.com>
2023-09-12 15:42:26 +0100

    usdc swap

--- blockchain/contracts/Usdc_Swap.sol
+++ blockchain/contracts/Usdc_Swap.sol
@@ -5,25 +5,22 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {CallOracle} from "./CallOracle.sol";
 
 interface IERC20 {
-    function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 /// @title A very simple lottery contract
 /// @author josevazf
 /// @notice You can use this contract for running a very simple lottery
 contract USDC_Swap is Ownable {
-    /// @notice Address of the token used to distribute rewards to lenders
+    /// @notice Address of the oracle contract ETH/USD from Chainlink feed
     CallOracle public oracleContract;
+    /// @notice Address of the USDC token contract from Aave
     IERC20 public usdcToken;
-
-    /// @notice Amount of USDC tokens in the prize pool
+    /// @notice Amount of USDC tokens in the pool
     uint256 public usdcPool;
 
     /// @notice Constructor function
@@ -32,10 +29,17 @@ contract USDC_Swap is Ownable {
         usdcToken = IERC20(0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8);
     }
 
+    /// @notice Get USDC balance in the contract
     function getUSDCBalance(address account) public view returns (uint256) {
         return usdcToken.balanceOf(account);
     }
 
+    /// @notice Get ETH balance in the contract
+    function getETHBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /// @notice Get ETH/USD price from the Oracle feed
     function getETHPrice() public view returns (int256) {
         return oracleContract.getEthUsdPrice();
     }
@@ -46,7 +50,7 @@ contract USDC_Swap is Ownable {
         usdcToken.transferFrom(msg.sender, address(this), amount);
     }
 
-    /// @notice Gives tokens based on the amount of ETH sent
+    /// @notice Gives tokens based on the amount of ETH sent and the price received from the Oracle
     /// @dev This implementation is prone to rounding problems
     function swapToUSDC() external payable {
         uint256 price = uint256(getETHPrice()) / 100;
@@ -55,7 +59,7 @@ contract USDC_Swap is Ownable {
         usdcToken.transfer(msg.sender, usdcAmount);
     }
 
-    /// @notice Withdraws `amount` of USDC from the pool
+    /// @notice Withdraws `amount` of USDC to the owner
     function ownerWithdrawUSDC(uint256 amount) external onlyOwner {
         require(amount <= usdcPool, "Amount not available to withdraw");
         usdcPool -= amount;
@@ -67,11 +71,4 @@ contract USDC_Swap is Ownable {
         (bool ok,) = msg.sender.call{value: address(this).balance}("");
         require(ok, "Failed to withdraw");
     }
-
-/*
-    /// @notice Burns `amount` tokens and give the equivalent ETH back to user
-    function returnTokens(uint256 amount) external {
-        paymentToken.burnFrom(msg.sender, amount);
-        payable(msg.sender).transfer(amount / purchaseRatio);
-    } */
 }
