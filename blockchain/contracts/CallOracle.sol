0dd02e1d2154c18d676bdece0cf581053d0b204b
jrocha-v <jrocha-v@student.42porto.com>
2023-09-13 02:15:31 +0100

    oracle contract

--- blockchain/contracts/CallOracle.sol
+++ blockchain/contracts/CallOracle.sol
@@ -1,34 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+  pragma solidity ^0.8.7;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+  import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract CallOracle {
-    AggregatorV3Interface internal dataFeed;
+  contract CallOracle {
+  AggregatorV3Interface internal dataFeed;
 
-    /**
-     * Network: Sepolia
-     * Aggregator: ETH/USD
-     * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-     */
-    constructor() {
-        dataFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
+  /**
+   * Network: Sepolia
+   * Aggregator: ETH/USD
+   * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+   */
+  constructor() {
+    dataFeed = AggregatorV3Interface(
+      0x694AA1769357215DE4FAC081bf1f309aDC325306
+    );
+  }
+
+  /**
+   * Returns the latest answer.
+   */
+  function getEthUsdPrice() public view returns (int256) {
+    // prettier-ignore
+    (
+      /* uint80 roundID */,
+      int256 answer,
+      /*uint startedAt*/,
+      /*uint timeStamp*/,
+      /*uint80 answeredInRound*/
+    ) = dataFeed.latestRoundData();
+      return answer;
     }
-
-    /**
-     * Returns the latest answer.
-     */
-    function getEthUsdPrice() public view returns (int256) {
-        // prettier-ignore
-        (
-            /* uint80 roundID */,
-            int256 answer,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-        ) = dataFeed.latestRoundData();
-        return answer;
-    }
-}
+  }
