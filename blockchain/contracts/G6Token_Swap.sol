f27bc8d3cc4ab1b218cb07a508025a582bf0ba3e
jrocha-v <jrocha-v@student.42porto.com>
2023-09-13 02:16:05 +0100

    g6t swap

--- blockchain/contracts/G6Token_Swap.sol
+++ blockchain/contracts/G6Token_Swap.sol
@@ -8,37 +8,37 @@ import {G6Token} from "./G6Token.sol";
 /// @author josevazf
 /// @notice This is the contract to deal with the G6 Token Swaps
 contract G6Token_Swap is Ownable {
-    /// @notice Address of the token used to distribute rewards to lenders
-    G6Token public g6Token;
-    /// @notice Amount of tokens given per ETH paid
-    uint256 public purchaseRatio;
+  /// @notice Address of the token used to distribute rewards to lenders
+  G6Token public g6Token;
+  /// @notice Amount of tokens given per ETH paid
+  uint256 public purchaseRatio;
 
-    /// @notice Constructor function
-    /// @param _tokenAddress Group 6 Token contract address
-    /// @param _purchaseRatio Amount of tokens given per ETH paid
-    constructor(
-        address _tokenAddress,
-        uint256 _purchaseRatio
-    ) {
-        g6Token = G6Token(_tokenAddress);
-        purchaseRatio = _purchaseRatio;
-    }
+  /// @notice Constructor function
+  /// @param _tokenAddress Group 6 Token contract address
+  /// @param _purchaseRatio Amount of tokens given per ETH paid
+  constructor(
+    address _tokenAddress,
+    uint256 _purchaseRatio
+  ) {
+    g6Token = G6Token(_tokenAddress);
+    purchaseRatio = _purchaseRatio;
+  }
 
-    /// @notice Gives tokens based on the amount of ETH sent
-    /// @dev This implementation is prone to rounding problems
-    function buyTokens() external payable {
-        g6Token.mint(msg.sender, msg.value * purchaseRatio);
-    }
+  /// @notice Gives tokens based on the amount of ETH sent
+  /// @dev This implementation is prone to rounding problems
+  function buyTokens() external payable {
+    g6Token.mint(msg.sender, msg.value * purchaseRatio);
+  }
 
-    /// @notice Burns `amount` tokens and give the equivalent ETH back to user
-    function sellTokens(uint256 amount) external {
-        g6Token.burnFrom(msg.sender, amount);
-        payable(msg.sender).transfer(amount / purchaseRatio);
-    }
+  /// @notice Burns `amount` tokens and give the equivalent ETH back to user
+  function sellTokens(uint256 amount) external {
+    g6Token.burnFrom(msg.sender, amount);
+    payable(msg.sender).transfer(amount / purchaseRatio);
+  }
 
-    /// @notice Whithdraws collected ETH to the owner
-    function withdraw() public onlyOwner{
-        (bool ok,) = msg.sender.call{value: address(this).balance}("");
-        require(ok, "Failed to withdraw");
-    }
+  /// @notice Whithdraws collected ETH to the owner
+  function withdraw() public onlyOwner{
+    (bool ok,) = msg.sender.call{value: address(this).balance}("");
+    require(ok, "Failed to withdraw");
+  }
 }
