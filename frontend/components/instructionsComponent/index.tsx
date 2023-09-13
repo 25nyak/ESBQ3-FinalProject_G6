40b667b97b38c44933eba43095248ac017127b27
jrocha-v <jrocha-v@student.42porto.com>
2023-09-13 02:17:21 +0100

    added token abis

--- frontend/components/instructionsComponent/index.tsx
+++ frontend/components/instructionsComponent/index.tsx
@@ -2,14 +2,16 @@ import { useState, useEffect } from 'react';
 import styles from './instructionsComponent.module.css';
 import { useAccount, useBalance, useContractRead, useContractWrite, useNetwork, usePrepareContractWrite, useWaitForTransaction } from 'wagmi';
 import { ethers} from 'ethers';
-import * as tokenJson from '../assets/LotteryToken.json';
+import * as g6TokenJson from '../assets/G6Token.json';
+import * as usdcTokenJson from '../assets/USDCToken.json';
+
 import Footer from "@/components/instructionsComponent/navigation/footer";
 
-const TOKEN_ADDRESS = '0xdCf3F6153F328A7Aacd7C688Bf39E8750a375746'; // 18 decimals
+const G6T_ADDRESS = '0xdCf3F6153F328A7Aacd7C688Bf39E8750a375746';   // 18 decimals
 const USDC_ADDRESS = '0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8'   //  6 decimals
 const ORACLE_ETH_USD = '0xD17ecb6579cAD73aE27596929e13b619bA9060A5' //  8 decimals
 const G6T_SWAP_CONTRACT = '0x2fF3113240Cd16199de9383939E3702b2e37d7c9'
-
+const USDC_SWAP_CONTRACT = '0x92b3c8E27921aD63A5859f8Bd266C6Fb6410b20A'
 
 export default function Loading() {
 	const [mounted, setMounted] = useState(false);
@@ -22,7 +24,7 @@ export default function Loading() {
 			<div className={styles.container}>
 				<header className={styles.header_container}>
 					<div className={styles.header}>
-						<span><h2>Lending Protocol</h2></span>
+						<span><h2>G6 Lending Protocol</h2></span>
 					</div>
 				</header>
 					<p className={styles.get_started}>
@@ -74,8 +76,10 @@ function UserInfo() {
 					</div>
 				</header>
 					<p>Connected to <i>{chain?.name}</i> network </p>
-					<TokenName></TokenName>
-					<WalletTokenBalance address={address}></WalletTokenBalance>
+					{/* <G6TokenName></G6TokenName> */}
+					<G6TokenBalance address={address}></G6TokenBalance>
+          {/* <USDCTokenName></USDCTokenName> */}
+					<USDCTokenBalance address={address}></USDCTokenBalance>
       </div>
     );
   if (isConnecting)
@@ -97,10 +101,10 @@ function UserInfo() {
   );
 }
 
-function TokenName() {
+function G6TokenName() {
   const { data, isError, isLoading } = useContractRead({
-    address: TOKEN_ADDRESS,
-    abi: tokenJson.abi,
+    address: G6T_ADDRESS,
+    abi: g6TokenJson.abi,
     functionName: "name",
   });
 
@@ -108,13 +112,13 @@ function TokenName() {
 
   if (isLoading) return <div>Fetching name…</div>;
   if (isError) return <div>Error fetching name</div>;
-  return <div><b>Token: </b> {name} ({TokenSymbol()})</div>;
+  return <div><b>Token: </b> {name} ({G6TokenSymbol()})</div>;
 }
 
-function TokenSymbol() {
+function G6TokenSymbol() {
   const { data, isError, isLoading } = useContractRead({
-    address: TOKEN_ADDRESS,
-    abi: tokenJson.abi,
+    address: G6T_ADDRESS,
+    abi: g6TokenJson.abi,
     functionName: 'symbol',
   });
 
@@ -125,14 +129,54 @@ function TokenSymbol() {
   return symbol;
 }
 
-function WalletTokenBalance(params: { address: `0x${string}` }) {
+function G6TokenBalance(params: { address: `0x${string}` }) {
   const { data, isError, isLoading } = useBalance({
     address: params.address,
-		token: TOKEN_ADDRESS,
+		token: G6T_ADDRESS,
 		watch: true
   });
 
   if (isLoading) return <div>Fetching balance…</div>;
   if (isError) return <div>Error fetching balance</div>;
-  return <div><b>Balance: </b>{data?.formatted} <TokenSymbol></TokenSymbol></div>;
+  return <div><b><G6TokenSymbol></G6TokenSymbol> balance: </b>{data?.formatted}</div>;
+}
+
+function USDCTokenName() {
+  const { data, isError, isLoading } = useContractRead({
+    address: USDC_ADDRESS,
+    abi: g6TokenJson.abi,
+    functionName: "name",
+  });
+
+  const name = typeof data === "string" ? data : 0;
+
+  if (isLoading) return <div>Fetching name…</div>;
+  if (isError) return <div>Error fetching name</div>;
+  return <div><b>Token: </b> {name} ({G6TokenSymbol()})</div>;
+}
+
+function USDCTokenSymbol() {
+  const { data, isError, isLoading } = useContractRead({
+    address: USDC_ADDRESS,
+    abi: g6TokenJson.abi,
+    functionName: 'symbol',
+  });
+
+  const symbol = typeof data === 'string' ? data : 0;
+
+  if (isLoading) return <div>Fetching name…</div>;
+  if (isError) return <div>Error fetching symbol</div>;
+  return symbol;
+}
+
+function USDCTokenBalance(params: { address: `0x${string}` }) {
+  const { data, isError, isLoading } = useBalance({
+    address: params.address,
+		token: USDC_ADDRESS,
+		watch: true
+  });
+
+  if (isLoading) return <div>Fetching balance…</div>;
+  if (isError) return <div>Error fetching balance</div>;
+  return <div><b><USDCTokenSymbol></USDCTokenSymbol> balance: </b>{data?.formatted}</div>;
 }
