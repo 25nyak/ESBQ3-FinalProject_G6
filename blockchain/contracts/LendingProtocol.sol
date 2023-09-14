f2c17b3965ed8538dce0a81c5111114141aa4fba
jrocha-v <jrocha-v@student.42porto.com>
2023-09-14 11:45:04 +0100

    updated protocol

--- blockchain/contracts/LendingProtocol.sol
+++ blockchain/contracts/LendingProtocol.sol
@@ -28,10 +28,10 @@ contract LendingProtocol is Ownable {
   struct UserInfo {
     uint256 depositL_USDC;          // USDC deposited (Lending)
     uint256 time_depositL_USDC;     // Time of USDC deposit (Lending)
-    uint256 reward_depositL_G6T;    // G6T rewards from deposit (Lending)-------> TO DO!
-    uint256 reward_depositL_USDC;   // USDC rewards from protocol fees----------> TO DO!
+    uint256 reward_depositL_USDC;   // USDC rewards from deposit (Lending) given as G6T
     uint256 depositC_ETH;           // ETH deposited (Collateral)
-    uint256 time_depositC_ETH;      // Time of ETH deposit (Collateral)---------> TO DO!
+    uint256 time_depositC_ETH;      // Time of ETH deposit (Collateral)
+    uint256 reward_depositC_ETH;    // USDC rewards from deposit (Collateral) given as G6T
     uint256 availableB_USDC;        // Available USDC to borrow 80% of ETH value in USDC (Borrowing)
     uint256 withdrawB_USDC;         // USDC withdrawn (Borrowing)
     uint256 time_withdrawB_USDC;    // Time of USDC withdraw (Borrowing)
@@ -40,13 +40,12 @@ contract LendingProtocol is Ownable {
 
   /// @notice Mapping for the address of each user struct element
   mapping (address => UserInfo) public user;
-  /// @dev List of users addresses
-  address[] public userList;
   /// @notice Amount of tokens in the Lending Pool
   uint256 public lendingPool;
   /// @notice Amount of tokens in the Fees Pool
   uint256 public feesPool;
-
+  /// @notice Amount of USDC borrowed
+  uint256 public borrowTV;
   /// @notice Constructor function
   constructor() {
     g6Token = G6Token(0xb46b5C88464E2DCeE987f159f6cF1066B52A360D);
@@ -60,21 +59,6 @@ contract LendingProtocol is Ownable {
     _;
   }
 
-  /*   // @notice Function to check if an address exists in userList
-  function addressExists(address _addr) public view returns (bool) {
-    for (uint256 i = 0; i < userList.length; i++) {
-      if (userList[i] == _addr) {
-        return true;
-      }
-    }
-    return false;
-  }
-  // @notice Function to add an address to userList if it doesn't exist
-  function addAddressIf(address _addr) public {
-    if (!addressExists(_addr))
-      userList.push(_addr);
-  } */
-
   // @notice Gets ETH price from oracle
   function getETHPrice() public view returns (uint256 ethPrice) {
     return uint256(oracleContract.getEthUsdPrice()) / 100;
@@ -85,8 +69,8 @@ contract LendingProtocol is Ownable {
     return (amount * getETHPrice())/(1 ether);
   }
 
-  // @notice Gets USDC value for a given `amount`of G6T 
-    function getUSDCValue_G6T(uint256 amount) public view returns (uint256) {
+  // @notice Gets G6T value for a given `amount`of USDC 
+  function getG6TValue_USDC(uint256 amount) public view returns (uint256) {
     uint256 ethAmount = (amount / 10000);
     return (ethAmount * getETHPrice())/(1 ether);
   }
@@ -109,14 +93,14 @@ contract LendingProtocol is Ownable {
     return tempFee;
   }
 
-  /// @notice Returns the total G6T deposit rewards of passed `account`
-  function totalG6TRewards_L(address account) public view returns (uint256) {
+  /// @notice Returns the total USDC Lender deposit rewards of passed `account`
+  function totalUSDCRewards_L(address account) public view returns (uint256) {
     if (user[account].time_depositL_USDC == 0)
       return 0;
     uint256 elapsedTime = block.timestamp - user[account].time_depositL_USDC;
     // @dev Reward rate of 0.000005%-second | 0.432%-day | APR:157.68%
-    uint256 totalRewardsUSDC = user[account].reward_depositL_G6T + elapsedTime * (user[account].depositL_USDC / 200000);
-    return getUSDCValue_G6T(totalRewardsUSDC);
+    uint256 totalRewardsUSDC = user[account].reward_depositL_USDC + elapsedTime * (user[account].depositL_USDC / 200000);
+    return totalRewardsUSDC;
   }
 
   /// @notice Deposits USDC `amount` to the Lending Pool
@@ -124,54 +108,82 @@ contract LendingProtocol is Ownable {
   function depositUSDC_L(uint256 amount) external minValue(amount){
     if (msg.sender == owner()) {
       usdcToken.transferFrom(msg.sender, address(this), amount);
-      user[msg.sender].depositL_USDC += amount;
-      lendingPool += amount;
+    } else if (user[msg.sender].time_depositL_USDC != 0) {
+      user[msg.sender].reward_depositL_USDC += totalUSDCRewards_L(msg.sender);
+      usdcToken.transferFrom(msg.sender, address(this), amount);
+      user[msg.sender].time_depositL_USDC = block.timestamp;
     } else {
       usdcToken.transferFrom(msg.sender, address(this), amount);
-      user[msg.sender].depositL_USDC += amount;
       user[msg.sender].time_depositL_USDC = block.timestamp;
-      lendingPool += amount;
     }
+    user[msg.sender].depositL_USDC += amount;
+    lendingPool += amount;
   }
 
   /// @notice Withdraws USDC `amount` from the Lending Pool
   /// @dev    Lender Dashboard
   function withdrawUSDC_L(uint256 amount) external minValue(amount){
     require(amount <= user[msg.sender].depositL_USDC, "Can't withdraw more than what you deposited");
+    uint256 rewards = getG6TValue_USDC(totalUSDCRewards_L(msg.sender));
+    if (amount == user[msg.sender].depositL_USDC)
+      user[msg.sender].time_depositL_USDC = 0;
     user[msg.sender].depositL_USDC -= amount;
     lendingPool -= amount;
-    uint256 rewards = totalG6TRewards_L(msg.sender);
-    user[msg.sender].reward_depositL_G6T = 0;
+    user[msg.sender].reward_depositL_USDC = 0;
     user[msg.sender].time_depositL_USDC = block.timestamp;
     usdcToken.transfer(msg.sender, amount);
     g6Token.mint(msg.sender, rewards);
+  }
+
+  /// @notice Returns the total USDC Collateral deposit rewards of passed `account`
+  /// @dev    Borrower Dashboard
+  function totalUSDCRewards_C(address account) public view returns (uint256) {
+    if (user[account].time_depositC_ETH == 0)
+      return 0;
+    uint256 elapsedTime = block.timestamp - user[account].time_depositC_ETH;
+    // @dev Reward rate of 0.0000025%-second | 0.216%-day | APR:78.84%
+    uint256 totalRewardsUSDC = user[account].reward_depositC_ETH + elapsedTime * (getUSDCValue_ETH(user[account].depositC_ETH) / 400000);
+    return totalRewardsUSDC;
   }
 
   /// @notice Deposits ETH tokens as Collateral and sets borrowing limit to 80% of ETH value in USDC
   /// @dev    Borrower Dashboard
   /// @dev This implementation is prone to rounding problems
   function depositETH_C() external payable {
+    if (user[msg.sender].time_depositC_ETH != 0)
+      user[msg.sender].reward_depositC_ETH += totalUSDCRewards_C(msg.sender);
+    user[msg.sender].time_depositC_ETH = block.timestamp;
     user[msg.sender].depositC_ETH += msg.value;
-    uint256 usdcValue = getUSDCValue_ETH(user[msg.sender].depositC_ETH);
-    user[msg.sender].availableB_USDC += (usdcValue * 8) / 10;
+    uint256 usdcC_Value = (getUSDCValue_ETH(user[msg.sender].depositC_ETH) * 8) / 10;
+    user[msg.sender].availableB_USDC = usdcC_Value - user[msg.sender].withdrawB_USDC;
   }
 
-  /// @notice Whithdraws collected ETH to the owner
-  function withdrawETH_C() external {
-    require(user[msg.sender].withdrawB_USDC == 0, "...");
-    (bool ok,) = msg.sender.call{value: user[msg.sender].depositC_ETH}("");
+  /// @notice Whithdraws `amount` of deposited ETH to the owner
+  function withdrawETH_C(uint256 amount) external minValue(amount){
+    require(amount <= user[msg.sender].depositC_ETH, "Can't withdraw more than what you deposited");
+    uint256 ethLeft = user[msg.sender].depositC_ETH - amount;
+    require(((getUSDCValue_ETH(ethLeft) * 8) / 10) >= totalDebtOf(msg.sender), "Current debt is above 80% of collateral value left");
+    uint256 rewards = getG6TValue_USDC(totalUSDCRewards_C(msg.sender));
+    if (amount == user[msg.sender].depositC_ETH)
+      user[msg.sender].time_depositC_ETH = 0;
+    user[msg.sender].reward_depositC_ETH = 0;
+    user[msg.sender].time_depositC_ETH = block.timestamp;
+    g6Token.mint(msg.sender, rewards);
+    (bool ok,) = msg.sender.call{value: amount}("");
     require(ok, "Failed to withdraw");
   }
 
   /// @notice Borrow USDC `amount` from the Lending Pool
   /// @dev    Borrower Dashboard
   function borrowUSDC_B(uint256 amount) external minValue(amount){
-    require(amount <= user[msg.sender].availableB_USDC, "Can't withdraw more than allowed");
+    require(amount <= user[msg.sender].availableB_USDC, "Can't borrow more than allowed");
+    require(amount <= ((lendingPool * 5 )/ 10), "Can't borrow more than 50% of the Lending Pool");
     if (user[msg.sender].interestFee != 0)
       user[msg.sender].interestFee = totalFeeOf(msg.sender);
     user[msg.sender].availableB_USDC -= amount;
     user[msg.sender].withdrawB_USDC += amount;
-    lendingPool -= amount;  
+    lendingPool -= amount;
+    borrowTV += amount;
     user[msg.sender].time_withdrawB_USDC = block.timestamp;
     usdcToken.transfer(msg.sender, amount);
   }
@@ -187,6 +199,7 @@ contract LendingProtocol is Ownable {
       user[msg.sender].availableB_USDC += (getUSDCValue_ETH(user[msg.sender].depositC_ETH) * 8) / 10;
       lendingPool += user[msg.sender].withdrawB_USDC;
       feesPool += user[msg.sender].interestFee;
+      borrowTV -= user[msg.sender].withdrawB_USDC;
       user[msg.sender].withdrawB_USDC = 0;
       user[msg.sender].interestFee = 0;
       user[msg.sender].time_withdrawB_USDC = 0;
@@ -198,6 +211,7 @@ contract LendingProtocol is Ownable {
       user[msg.sender].interestFee = 0;
       user[msg.sender].availableB_USDC += amountDif;
       lendingPool += amountDif;
+      borrowTV -= amountDif;
       user[msg.sender].withdrawB_USDC -= amountDif;
     } else { // if amount is less then interestFee
       usdcToken.transferFrom(msg.sender, address(this), amount);
@@ -206,25 +220,21 @@ contract LendingProtocol is Ownable {
     }
   }
 
-  /** //////                  FOR TESTING                   \\\\\\\
-  *
-  */
-  /// @notice Whithdraws collected ETH to the owner
+  /// @notice Whithdraws deposited ETH to the owner
+  /// @dev    Just for testing purposes, should not exist
   function ownerWithdrawETH() external onlyOwner{
     (bool ok,) = msg.sender.call{value: address(this).balance}("");
     require(ok, "Failed to withdraw");
   }
 
-  /// @notice Withdraws USDC `amount` from the Lending Pool
-  /// @dev    Lender Dashboard
+  /// @notice Withdraws all USDC to the owner
+  /// @dev    Just for testing purposes, should not exist
   function ownerWithdrawUSDC() external onlyOwner{
     usdcToken.transfer(msg.sender, lendingPool + feesPool);
     lendingPool = 0;
+    feesPool = 0;
   }
+
   /// @notice Receive function allows to receive ETH when no calldata is passed
   receive() external payable {}
-  /*     
-  *
-  ** \\\\\\                  FOR TESTING                   ////// */
-
 }
